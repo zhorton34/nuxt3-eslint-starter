37009eb1e118c258aa7b7730fa3131cbcd2c35b9
Zak Horton <zhorton999@gmail.com>
2022-08-31 00:20:27 -0700

    test

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  typescript: {
+    strict: true,
+  },
+  modules: ['@vueuse/nuxt', '@pinia/nuxt'],
+  vueuse: {
+    ssrHandlers: true,
+  },
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  css: ['~/assets/css/tailwind.css'],
+})
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'empty',
+})
+
+useHead({
+  title: 'Nuxt3 Eslint',
+})
+</script>
+
+<template>
+  <div>
+    <WelcomeComponent />
+  </div>
+</template>
